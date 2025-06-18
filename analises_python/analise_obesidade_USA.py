fdb62f82edeffb2b1a5055b66a9c8165ec49006f
Khalilkma <147070933+Khalilkma@users.noreply.github.com>
2025-06-18 19:20:54 -0300

    Média de Obesidade, moda e mediana

--- analises_python/analise_obesidade_USA.py
+++ analises_python/analise_obesidade_USA.py
@@ -5,3 +5,11 @@ import matplotlib as plt
 
 df = pd.read_csv('../arquivos_csv/obesidade_estados.csv')
 print(df.head())
+
+media = df['Obesity'].mean()
+mediana = df['Obesity'].median()
+moda = df['Obesity'].mode()[0]
+
+print(f"Média de Obesidade: {media:.2f}%")
+print(f"Mediana: {mediana}%")
+print(f"Moda: {moda}%")
