c00202b5c7899d56f419e8cc5a3695d5d19911ba
Khalilkma <147070933+Khalilkma@users.noreply.github.com>
2025-06-18 19:27:40 -0300

    Cálculos de dispersão

--- analises_python/analise_obesidade_USA.py
+++ analises_python/analise_obesidade_USA.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+from matplotlib import pyplot as plt
 
 
 df = pd.read_csv('../arquivos_csv/obesidade_estados.csv')
 print(df.head())
 
+# Medidas de tendência central
 media = df['Obesity'].mean()
 mediana = df['Obesity'].median()
 moda = df['Obesity'].mode()[0]
@@ -13,3 +14,12 @@ moda = df['Obesity'].mode()[0]
 print(f"Média de Obesidade: {media:.2f}%")
 print(f"Mediana: {mediana}%")
 print(f"Moda: {moda}%")
+
+# Cálculos de dispersão
+desvio_padrao = df['Obesity'].std()
+variancia = df['Obesity'].var()
+
+print(f"Desvio Padrão: {desvio_padrao:.2f}")
+print(f"Variância: {variancia:.2f}")
+
+# Histograma (Fazer)
